64eb7361148746df0cfb6721d4e30b54b701942e
Robine Savert <rsavert@enrise.com>
2020-11-03 15:30:12 +0100

    added backend

---
+++ crypto-backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const crypto = require('./crypto.json');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+const port = 1337;
+
+app.get('/api/crypto', (req, res) => {
+    res.send(crypto);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
